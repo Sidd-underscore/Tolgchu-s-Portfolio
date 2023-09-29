f37d5c45efcace53f4bd9ee0a8ffd04eb6e67d5d
Sidd_ <sdmdevelopment3@gmail.com>
2023-09-29 16:23:22 +0000

    fix image

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
 const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lanyard.cnrad.dev',
-        port: '1452',
+        port: '',
         pathname: '**',
       },
     ],
